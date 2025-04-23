bdd1939c601b294ba063772fbf84d02d724ea0da
saladin <solehadinmoh@gmail.com>
2025-04-23 07:01:39 +0300

    implement list product functionality

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -49,6 +49,14 @@ const addProduct = async (req,res) => {
 //function for listing product
 
 const listProduct = async (req,res) => {
+    try {
+        const products = await productModel.find({});
+        res.json({success:true,products})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false , message:error.message})
+        
+    }
     
 }
 
