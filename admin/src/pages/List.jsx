638e1d54f530479ac0bcf6911a7bbf6080496f09
saladin <solehadinmoh@gmail.com>
2025-04-26 07:22:01 +0300

    implement product listing functionality with images and basic details

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { backendUrl, currency } from "../App";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
-  return <div>list</div>;
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      if (response.data.success) {
+        console.log(response.data)
+        setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+  return (
+    <>
+      <p className="mb-2">All Products List</p>
+      <div className="flex flex-col gap-2">
+        {/* ------------List Table Title------------- */}
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border border-gray-200 bg-gray-200 text-sm">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className="text-center">Action</b>
+        </div>
+
+        {/* ------Product List --------- */}
+
+        {
+          list.map((item,index)=>(
+            <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border border-gray-200 text-sm" key={index}>
+               <img className="w-12" src={item.images[0]} alt="" />
+               <p>{item.name}</p>
+               <p>{item.category}</p>
+               <p>{currency}{item.price}</p>
+               <p className="text-right md:text-center text-lg cursor-pointer">X</p>
+            </div>
+          ))
+        }
+      </div>
+    </>
+  );
 };
 
 export default List;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,7 +9,7 @@ const productRouter = express.Router();
 
 productRouter.post('/add',adminAuth,upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1},]),addProduct);
 productRouter.post('/remove',adminAuth,removeProduct);
-productRouter.post('/list',listProduct);
+productRouter.get('/list',listProduct);
 productRouter.post('/single',singleProduct);
 
 
